833d61b5e0fd54639272e7239d0edac1eb34f65b
Rashmi <87723006+rashmibakkolla@users.noreply.github.com>
2024-07-11 19:37:44 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3
+    +3) //code readability should be high
+console.log("Rashmi")
+
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value and also null is an object 
+// undefined => the value is not defined
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof "rashmi");
+console.log(typeof null); //type is object
+console.log(typeof undefined); // type is undefined
